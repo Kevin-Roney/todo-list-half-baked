6139fbe59c5bbf9926e81da51fcdb3f2d962b8e2
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-28 14:07:09 -0700

    finished renderTodo function in render-utils

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,19 @@
 export function renderTodo(todo) {
     // create a div and a p tag
+    const todoEl = document.createElement('div');
+    const todoItem = document.createElement('p');
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-
+    if (todo.complete === true) {
+        todoEl.classList.add('complete');
+    } else {
+        todoEl.classList.add('incomplete');
+    }
     // add the 'todo' css class no matter what
-
+    todoEl.classList.add('todo');
     // put the todo's text into the p tag
-
+    todoItem.textContent = todo.todo;
     // append stuff
-
+    todoEl.append(todoItem);
     // return the div
+    return todoEl;
 }
